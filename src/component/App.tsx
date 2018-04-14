a77d2eff02bf1fa4b4f7a5fba3e27105e55e357e
Jayson <jayson@mac.local>
2018-04-14 13:51:55 +0800

    setp 1

---
+++ src/component/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import '../css/App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './component/App';
+import './css/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
+);
+
